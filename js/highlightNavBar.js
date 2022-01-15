3d7b2847725a5b0b869d330a5214c35e81ea75c0
Nisanur Genc <81337529+gencnis@users.noreply.github.com>
2022-01-15 23:50:15 +0300

    Add files via upload

---
+++ js/highlightNavBar.js
@@ -0,0 +1,19 @@
+/*
+    This document is used in all of the pages, highlights the navigation bar items when the mouse is on them
+*/
+
+function highlightNav(x) {
+    x.style.color = "#B1B1B1";
+}
+
+function normalNav(x) {
+    x.style.color = "#F0F1F2"; //degisti
+}
+
+function highlightMenu(x) {
+    x.style.background = "#B1B1B1"; //degisti
+}
+
+function normalMenu(x) {
+    x.style.background = "#F0F1F2;"; //degisti
+}
